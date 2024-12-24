e683f25d61328b1d778b0c493a002e78bd01948d
srustamov <ya.mersedez@yandex.ru>
2024-12-24 13:07:07 +0600

    fix

--- src/main/java/com/example/lnggroup/LngGroupApplication.java
+++ src/main/java/com/example/lnggroup/LngGroupApplication.java
@@ -2,12 +2,11 @@ package com.example.lnggroup;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LngGroupApplication {
-
+public class LngGroupApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(LngGroupApplication.class, args);
     }
-
 }
--- src/main/java/com/example/lnggroup/controller/MainController.java
+++ src/main/java/com/example/lnggroup/controller/MainController.java
@@ -2,11 +2,18 @@ package com.example.lnggroup.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class MainController {
     @GetMapping("/lng-group")
-    public String mainController(){
+    public String mainController() {
         return "lng-group";
+    }
+
+    @RequestMapping("*")
+    public RedirectView redirect() {
+        return new RedirectView("/lng-group");
     }
 }
